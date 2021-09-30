61c948b67830e08fc0658cc4091dc5d4031c33a8
Opilane TTHK <opilane@tthk.ee>
2021-09-30 11:29:20 +0300

    More Loops

---
+++ GuessingGameInfinite/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GuessingGameInfinite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm genereerib juhusliku numbri 1-10
+            //kasutaja peab numbri ära arvama
+            //kui kasutaja arvab numbri ära, siis on ta võitnud
+            //katsete arv on piiramatu
+
+            Random random = new Random();
+            int correctNum = random.Next(1, 11);
+            bool loopActive = true;
+
+
+            while (loopActive)
+            {
+                Console.WriteLine("Vali number 1-10:");
+                int userNum = Convert.ToInt32(Console.ReadLine());
+
+                if (correctNum == userNum)
+                {
+                    Console.WriteLine("Palju Õnne, Oled võitnud:");
+                    loopActive = false;
+                }
+                else
+                {
+                    Console.WriteLine("Vale vastus, proovi uuesti:");
+                }
+            }
+            Console.WriteLine("Kena päeva!");
+            
+           
+
+
+            
+        }
+    }
+}
---
+++ GuessingGameThreeTries/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace GuessingGameThreeTries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int correctNum = random.Next(1, 11);
+
+            int i = 0;
+            while(i < 3)
+            {
+                Console.WriteLine("Vali number 1-10");
+                int userNum = Convert.ToInt32(Console.ReadLine());
+
+                if(userNum == correctNum)
+                {
+                    Console.WriteLine("Palju õnne, oled võitnud");
+                    break; //(i = 3)
+                }
+                else
+                {
+                    i = i + 1;
+                    Console.WriteLine($"Proovi uuesti. {3-i} katset on alles!");
+                }
+            }
+        }
+    }
+}
